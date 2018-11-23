ace270dd8a48975b4bb5c126fcfe80eb65e40c47
themanman999 <35715069+themanman999@users.noreply.github.com>
2018-11-23 13:23:48 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-"use strict";importScripts("sw-toolbox.js"),toolbox.precache(["index.html","assets/css/main.css","assets/css/font-awesome.min.css"]),toolbox.router.get("/images/*",toolbox.cacheFirst),toolbox.router.get("/*",toolbox.networkFirst,{networkTimeoutSeconds:5});
+"use strict";importScripts("sw-toolbox.js"),toolbox.precache(["index.html"]),toolbox.router.get("/images/*",toolbox.cacheFirst),toolbox.router.get("/*",toolbox.networkFirst,{networkTimeoutSeconds:5});
